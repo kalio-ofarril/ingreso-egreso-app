e922c7e7c8edb28425afdcfe0d9882cff68dffe8
Kalio <kalio@Kalios-Mac-mini.local>
2025-09-03 10:39:01 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./auth/login/login.component";
+import { RegisterComponent } from "./auth/register/register.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [RouterLink],
+  templateUrl: './sidebar.component.html',
+  styles: ``
+})
+export class SidebarComponent {
+
+}
